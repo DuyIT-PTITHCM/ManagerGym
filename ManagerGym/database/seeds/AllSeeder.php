fd43f467e346ff978935278ddb7e220cedc6dcc7
Nguyen Anh Duy <54759692+DuyIT-PTITHCM@users.noreply.github.com>
2021-05-08 17:05:31 +0700

    xong

--- ManagerGym/database/seeds/AllSeeder.php
+++ ManagerGym/database/seeds/AllSeeder.php
@@ -4,10 +4,9 @@ use App\Models\Department;
 use App\Models\Device;
 use App\Models\Partner;
 use App\Models\Purchasing;
-use App\Models\Service;
 use App\Models\Supplier;
 use App\Models\TypeMachine;
-use App\User;
+
 use Illuminate\Database\Seeder;
 
 class AllSeeder extends Seeder
@@ -33,9 +32,6 @@ class AllSeeder extends Seeder
            TypeMachine::create([
                 'name'=>$faker->name
            ]);
-           Service::create([
-                'name'=>$faker->name
-           ]);
            Partner::create([
             'name' => $faker->name,
             'address' => $faker->address,
@@ -51,12 +47,6 @@ class AllSeeder extends Seeder
             'name' => $faker->name,
             'address' => $faker->address,
             'phone'=>'0' . $faker->randomNumber(9)
-           ]);
-           User::create([
-            'name' => $faker->name,
-            'email' => $faker->unique()->email,
-            'phone'=>'0' . $faker->randomNumber(9),
-            'password'=>'12345678'
            ]);
         }
 
